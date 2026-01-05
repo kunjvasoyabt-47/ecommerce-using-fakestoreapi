8ef149a8fbb60849c3c0c147d0d4ce1cc92651c9
kunjvasoyabt-47 <kunj.v@bitontree.com>
2026-01-05 11:33:49 +0530

    setup base url in .env

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base URL configuration
 const api = axios.create({
-  baseURL: 'https://fakestoreapi.com',
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
